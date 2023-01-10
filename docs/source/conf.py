259d0ecba3886b3203dfdabb3d979333fc23cec1
eyndjl <eyndjl@eyndjl.debian>
2023-01-11 00:28:55 +0500

    Исправление очепяток

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Eybie Docs'
+copyright = '2023, Eyndjl'
+author = 'Eyndjl'
 
 release = '0.1'
 version = '0.1.0'
